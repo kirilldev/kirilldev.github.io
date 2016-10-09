73d54b9c595d8f8d89f25429d6131d4c59920f0e
annaisaykina <kreznykov@gmail.com>
2016-10-10 00:36:14 +0300

    update

---
+++ db/solutions/js/is-trees-are-mirrors.js
@@ -0,0 +1,26 @@
+/*
+function Node (data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+}
+*/
+
+//     10              10
+//     / \            /  \
+//    12  4    =>    4    12
+//   /    /           \     \
+//  5    1              1    5
+
+function isMirrorTrees (head1, head2) {
+	if (!head1 || !head2) {
+		return !head1 && !head2;
+	}
+  
+	if (head1.data !== head2.data) {
+		return false;
+	}
+ 
+	return isMirrorTrees(head1.left, head2.right) 
+  			&& isMirrorTrees(head1.right, head2.left);
+}
