9cb9bb5a34859af5cc4b06715e5d1e1f0b092854
annaisaykina <kreznykov@gmail.com>
2016-10-18 00:14:17 +0300

    upd

---
+++ db/solutions/js/create-bst-from-sorted-array.js
@@ -0,0 +1,24 @@
+/*
+ * function Node(val) {
+ *     this.val = val;
+ *     this.left =  null;
+ *     this.right = null'
+ * }
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {Node}
+ */
+var sortedArrayToBST = function(nums, start=0, end=nums.length-1) {
+    if (start > end) {
+        return null
+    }
+
+    let mid = (end + start) / 2;
+    let root = new Node(nums[mid]);
+    root.left = sortedArrayToBST(nums, start, mid - 1);
+    root.right = sortedArrayToBST(nums, mid + 1, end);
+
+    return root;
+};
