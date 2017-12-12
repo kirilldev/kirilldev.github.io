c788231755143d24142f63f570073e5d5d562fcd
Kirill Reznykov <kreznykov@gmail.com>
2017-12-12 17:15:32 +0300

    update

--- src/app/components/problem/app.problemComponent.js
+++ src/app/components/problem/app.problemComponent.js
@@ -1,62 +1,82 @@
 import {Component} from '@angular/core'
 import SolutionLoaderService from '../../services/solutionLoader.service';
 
+// TODO:
+const colorMapping = {
+    'js': 'red',
+    'java': 'blue',
+    'py': 'green',
+    'scala': 'black'
+};
+
 var ProblemComponent =
-        Component({
-            selector: 'problem',
-            templateUrl: 'src/app/components/problem/problemHtml.html',
-            providers: [SolutionLoaderService],
-            inputs: [
-                'problem : problem'
-            ]
-        }).Class({
-            constructor: [SolutionLoaderService, function(solutionLoader) {
-                this.problem = null;
-                this.solutionLoader = solutionLoader;
+    Component({
+        selector: 'problem',
+        templateUrl: 'src/app/components/problem/problemHtml.html',
+        providers: [SolutionLoaderService],
+        inputs: [
+            'problem : problem'
+        ]
+    }).Class({
+        constructor: [SolutionLoaderService, function (solutionLoader) {
+            this.problem = null;
+            this.solutionLoader = solutionLoader;
 
-                this.check = (e) => {
-                    e.preventDefault();
-                    var href = e.target.href;
+            this.check = (e) => {
+                e.preventDefault();
+                const href = e.target.href;
 
-                    if (href) {
-                        this.showModal = '<img src="' + href +'"/>';
-                    }
-                };
+                if (href) {
+                    this.showModal = '<img src="' + href + '"/>';
+                }
+            };
 
-                this.close = () => this.showModal = false;
-            }],
+            this.close = () => this.showModal = false;
+        }],
 
-            showSolution : function () {
-                this.solutionLoader
-                    .load(this.problem.solutions[0])
-                    .subscribe(c => this.showModal = c);
-            },
+        getSolutionsList: function (problem) {
+            return (problem.solutions || []).map(path => {
+                const tag = path.substring(path.lastIndexOf('.') + 1);
 
-            copyToClipboard: function (id) {
-                const text = `${window.location.origin}?problem=${id}`;
+                return {
+                    tag: tag,
+                    color: colorMapping[tag] || 'grey',
+                    path: tag + '/' + path
+                }
+            }).sort((a, b) => (a.tag > b.tag) ? 1 : -1);
+        },
 
-                if (window.clipboardData && window.clipboardData.setData) {
-                    // IE specific code path to prevent textarea being shown while dialog is visible.
-                    return clipboardData.setData("Text", text);
+        showSolution: function ($event, path) {
+            $event.preventDefault();
 
-                } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-                    var textarea = document.createElement("textarea");
-                    textarea.textContent = text;
-                    textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
-                    document.body.appendChild(textarea);
-                    textarea.select();
-                    try {
-                        document.execCommand("copy");
-                        // Security exception may be thrown by some browsers.
-                        return alert(`Link copied! ${text}`);
-                    } catch (ex) {
-                        console.warn("Copy to clipboard failed.", ex);
-                        return false;
-                    } finally {
-                        document.body.removeChild(textarea);
-                    }
+            this.solutionLoader.load(path).subscribe(c => this.showModal = c);
+        },
+
+        copyToClipboard: function (id) {
+            const text = `${window.location.origin}?problem=${id}`;
+
+            if (window.clipboardData && window.clipboardData.setData) {
+                // IE specific code path to prevent textarea being shown while dialog is visible.
+                return clipboardData.setData("Text", text);
+
+            } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                var textarea = document.createElement("textarea");
+                textarea.textContent = text;
+                textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+                document.body.appendChild(textarea);
+                textarea.select();
+                try {
+                    document.execCommand("copy");
+                    // Security exception may be thrown by some browsers.
+                    return alert(`Link copied! ${text}`);
+                } catch (ex) {
+                    console.warn("Copy to clipboard failed.", ex);
+                    return false;
+                } finally {
+                    document.body.removeChild(textarea);
                 }
             }
-        });
+        }
+    });
 
 export default ProblemComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,20 @@
 'use strict';
 
-//const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-
-const NODE_ENV = (process.env.NODE_ENV || 'dev').trim().toLowerCase();
-const isDevEnv = () => NODE_ENV === 'dev';
-const isProdEnv = () => NODE_ENV === 'prod';
-
-/*        copyPluginPaths: [
-	{from: 'frontend/view/utility', to: 'WEB-INF'},
-	{from: 'frontend/fonts/open-sans-condensed.woff.gz', to: 'assets/open-sans-condensed.woff.gz'},
-	{from: 'frontend/img/favicon.png', to: 'assets/img/favicon.png'}
-]*/
-
-if (isDevEnv() || isProdEnv()) {
-	console.log("***** NODE_ENV=" + NODE_ENV + " *****")
-} else {
-	throw new Error("Unknown NODE_ENV '" + NODE_ENV + '\'');
-}
 
 const plugins = [
     new CleanWebpackPlugin(['/dist'], {
         verbose: true
     }),
     new webpack.NoErrorsPlugin()
-  //  new CopyWebpackPlugin(ENV_DATA[NODE_ENV].copyPluginPaths)
-    /*        new webpack.ProvidePlugin({
-     angular: 'frontend/js/thirdparty/angular.js'
-     })*/
 ];
 
-if (!isDevEnv()) {
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-        drop_console: true,
-        dead_code: true,
-        unsafe: true
-    }));
-}
+plugins.push(new webpack.optimize.UglifyJsPlugin({
+    drop_console: true,
+    dead_code: true,
+    unsafe: true
+}));
 
 module.exports = {
     entry: './src/app/main.js',
@@ -48,8 +25,8 @@ module.exports = {
     resolve: {
         extensions: ['', '.js']
     },
-    devtool: isDevEnv() ? 'eval' : null,
-    watch: isDevEnv(),
+    devtool: null,
+    watch: false,
     watchOptions: {
         aggregateTimeout: 200
     },
